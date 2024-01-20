f8b989d0c498c70027fe90128559ea4fadd36d52
Newton Vital <newtonvital2819@gmail.com>
2024-01-20 10:01:08 +0530

    Gotham city relies on one man and its Newton

---
+++ projectjanuary6/github216/src/github/ADD.java
@@ -0,0 +1,17 @@
+package github;
+
+public class ADD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int add;
+		int num1=3;
+		int num2=4;
+		
+		int sum=0;
+		sum=num1+num2;
+		System.out.println(sum);
+	}
+
+}
